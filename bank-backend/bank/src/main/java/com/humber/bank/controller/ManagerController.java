2c862508d595f5d73380cb77fb374286dd8e5cc0
unknown <helypatel263@gmail.com>
2022-08-11 11:39:03 -0400

    Transaction and Branch service added

--- bank-backend/bank/src/main/java/com/humber/bank/controller/ManagerController.java
+++ bank-backend/bank/src/main/java/com/humber/bank/controller/ManagerController.java
@@ -1,6 +1,5 @@
 package com.humber.bank.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
@@ -8,8 +7,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,25 +15,22 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
-
 
 import com.humber.bank.entity.Account;
-import com.humber.bank.entity.Branch;
-import com.humber.bank.entity.CurrentAccount;
 import com.humber.bank.entity.Customer;
 import com.humber.bank.entity.Transaction;
 import com.humber.bank.exception.ResourceNotFoundException;
 import com.humber.bank.service.AccountService;
-// import com.humber.bank.service.BranchService;
 import com.humber.bank.service.CustomerService;
 import com.humber.bank.service.TransactionService;
 
 @RestController
 @RequestMapping("/api/manager")
 public class ManagerController {
+	
+	@Autowired
+	TransactionService transactionService;
 
 	@Autowired
 	AccountService accountService;
@@ -59,7 +53,38 @@ public class ManagerController {
 
 	}
 
+    @PostMapping("/withdraw")
+	public ResponseEntity<?> withdraw(@RequestBody Transaction transaction) {
+		try {
+			Map<String, String> withdrawMap = transactionService.withdraw(transaction);
+			return !withdrawMap.isEmpty() ? new ResponseEntity<>(withdrawMap, HttpStatus.OK)
+					: new ResponseEntity<String>("Insufficient Balance", HttpStatus.BAD_REQUEST);
+		} catch (Exception exception) {
+			throw new ResourceNotFoundException(exception.getMessage());
+		}
+	}
 
+	@PostMapping("/fundTransfer")
+	public ResponseEntity<?> fundTransfer(@RequestBody Transaction transaction) {
+		try {
+			Map<String, String> fundTransferMap = transactionService.fundTransfer(transaction);
+			return !fundTransferMap.isEmpty() ? new ResponseEntity<>(fundTransferMap, HttpStatus.OK)
+					: new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		} catch (Exception exception) {
+			throw new ResourceNotFoundException("Fund transfer failed");
+		}
+	}
+
+	@PostMapping("/deposit")
+	public ResponseEntity<?> deposit(@RequestBody Transaction transaction) {
+		try {
+			Map<String, String> depositMap = transactionService.deposit(transaction);
+			return !depositMap.isEmpty() ? new ResponseEntity<>(depositMap, HttpStatus.OK)
+					: new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		} catch (Exception exception) {
+			throw new ResourceNotFoundException(exception.getMessage());
+		}
+	}
 
 	@PostMapping("/addCustomer")
 	public ResponseEntity<?> addCustomer(@RequestBody Customer customer) {
